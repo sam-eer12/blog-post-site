65c83098eb7f82b2ed683e35d849fd18d1d198f5
Sameer Gupta <sameer.gupta.ug24@nsut.ac.in>
2025-07-21 20:44:16 +0530

    not required

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -9,10 +9,15 @@ const Layout = () => {
     }
   return (
     <>
-      <div>
+      <div className='flex justify-between items-center py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
         <img src="/image.png" alt="Logo" className='w-45 h-30 cursor-pointer' 
         onClick={()=>navigate('/')} />
         <button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'>Log out</button>
+      </div>
+      <div className='flex h-[calc(100vh-70px)]'>
+        <div>
+          
+        </div>
       </div>
     </>
   )
